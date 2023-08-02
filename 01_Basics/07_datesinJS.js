d10305403c5c9864ae11e45bbabc6182251a7445
Dhumdeo-G <140549715+Dhumdeo-G@users.noreply.github.com>
2023-08-02 11:53:15 +0000

    Dates Changes

--- 01_Basics/07_datesinJS.js
+++ 01_Basics/07_datesinJS.js
@@ -3,7 +3,7 @@
 let myDate = new Date()
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
+ console.log(myDate.toLocaleString());
 // console.log(typeof myDate);
 
 // let myCreatedDate = new Date(2023, 0, 23)
@@ -14,9 +14,9 @@ let myCreatedDate = new Date("01-14-2023")
 
 let myTimeStamp = Date.now()
 
-// console.log(myTimeStamp);
+//console.log(myTimeStamp); //dates in seconds
 // console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+ //console.log(Math.floor(Date.now()/1000)); // dated in seconds
 
 let newDate = new Date()
 console.log(newDate);
@@ -25,7 +25,7 @@ console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
 
-newDate.toLocaleString('default', {
+console.log(newDate.toLocaleString('default', {
     weekday: "long",
     
-})
+}))
