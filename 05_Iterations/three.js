308fb1a410281fe5341212ddf536771c353eaa31
Dhumdeo-G <140549715+Dhumdeo-G@users.noreply.github.com>
2023-08-08 08:24:14 +0000

    iteration js

--- 05_Iterations/three.js
+++ 05_Iterations/three.js
@@ -2,11 +2,11 @@
 // for of loop
 
 // ["", "", ""]
-// [{}, {}, {}]
+// [{}, {}, {}]  // array of an objects
 
 const arr = [1, 2, 3, 4, 5]
 
-for (const num of arr) {
+for (const num of arr) {  // const variable of Array/string
     //console.log(num);
 }
 
@@ -16,7 +16,9 @@ for (const greet of greetings) {
 }
 
 // Maps
-
+// Map will give you new Array
+// it maintained the order of insertion
+// key - value
 const map = new Map()
 map.set('IN', "India")
 map.set('USA', "United States of America")
@@ -26,7 +28,7 @@ map.set('IN', "India")
 
 // console.log(map);
 
-for (const [key, value] of map) {
+for (const [key, value] of map) { // destructuring of a map
     // console.log(key, ':-', value);
 }
 
@@ -35,7 +37,7 @@ const myObject = {
     game2: 'Spiderman'
 }
 
-// for (const [key, value] of myObject) {
+// for (const [key, value] of myObject) {  // you can not iterate object with for of loop 
 //     console.log(key, ':-', value);
     
 // }
