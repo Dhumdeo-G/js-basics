c0778cf7a91eb3d108e9298e6d25a46f4b18b1d0
Dhumdeo-G <140549715+Dhumdeo-G@users.noreply.github.com>
2023-08-08 13:46:11 +0000

    iteration js

---
+++ 05_Iterations/nine.js
@@ -0,0 +1,34 @@
+const myNums = [1, 2, 3]
+// parameters --- accumulator , currentvalue
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+// it return a single value
+console.log(myTotal);
+
+/ Adding prices from a cart.
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
